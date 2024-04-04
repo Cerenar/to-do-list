b0063bf8b7d59024645a343fb89de85770692670
Cerenar <camerondonner63@gmail.com>
2024-04-04 13:04:51 -0500

    Finish working application logic for the console

--- src/index.js
+++ src/index.js
@@ -3,4 +3,30 @@ import * as taskModule from './task.js';
 import { format } from 'date-fns';
 
 const date = format(new Date(), 'MM/dd/yyyy');
-console.table(taskModule.createTask('Eat', 'a lot', 5, date));
+const myTask = taskModule.createTask('Eat', 'a lot', 5, date, true);
+const myOtherTask = taskModule.createTask('Sleep', 'more', 3, date, false)
+const myThirdTask = taskModule.createTask('Game', 'more', 3, date, true)
+const myLastTask = taskModule.createTask('Last task', 'more', 3, date, false)
+const myLasTask = taskModule.createTask('Las task', 'more', 3, date, false)
+const myLaTask = taskModule.createTask('La task', 'more', 3, date, true)
+const myLTask = taskModule.createTask('L task', 'more', 3, date, false)
+const mTask = taskModule.createTask('m task', 'more', 3, date, true)
+const myLastaTask = taskModule.createTask('Lasta task', 'more', 3, date, false)
+
+const myProject = taskModule.createProject('myProject');
+
+taskModule.addTaskToProject(myProject, myTask);
+taskModule.addTaskToProject(myProject, myOtherTask);
+taskModule.addTaskToProject(myProject, myThirdTask);
+taskModule.addTaskToProject(myProject, myLastTask);
+taskModule.addTaskToProject(myProject, myLasTask);
+taskModule.addTaskToProject(myProject, myLaTask);
+taskModule.addTaskToProject(myProject, myLTask);
+taskModule.addTaskToProject(myProject, mTask);
+taskModule.addTaskToProject(myProject, myLastaTask);
+taskModule.updateTaskPriority(myTask, 4);
+taskModule.updateTaskCompleted(myLastTask, true);
+taskModule.deleteTask(myProject, myThirdTask);
+taskModule.clearCompletedTasks(myProject);
+
+console.log(myProject);
--- src/task.js
+++ src/task.js
@@ -1,11 +1,63 @@
-export default function task() {
+export function createProject (name) {
+    this.name = name;
+    const tasks = [];
 
+    return { name, tasks };
 }
 
-export function createTask (name, desc, priority, dueDate) {
+export function createTask (name, desc, priority, dueDate, completionStatus) {
     this.name = name;
     this.desc = desc;
     this.priority = priority;
     this.dueDate = dueDate;
-    return { name, desc, priority, dueDate };
+    this.completed = completionStatus;
+    return { name, desc, priority, dueDate, completionStatus };
+}
+
+export function addTaskToProject (project, task) {
+    project.tasks.push(task);
+    return project;
+}
+
+export function updateTaskName (task, newName) {
+    task.name = newName;
+    return task;
+}
+
+export function updateTaskDesc (task, newDesc) {
+    task.desc = newDesc;
+    return task;
+}
+
+export function updateTaskPriority (task, newPriority) {
+    task.priority = newPriority;
+    return task;
+}
+
+export function updateTaskDueDate (task, newDueDate) {
+    task.dueDate = newDueDate;
+    return task;
+}
+
+export function updateTaskCompleted (task, newCompletionStatus) {
+    task.completionStatus = newCompletionStatus;
+    return task;
+}
+
+export function deleteTask (project, task) {
+    for (let i = 0; i < project.tasks.length; i++) {
+        if (task === project.tasks[i]) {
+            project.tasks.splice(i, 1);
+        }
+    }
+    return project;
+}
+
+export function clearCompletedTasks (project) {
+    for (let i = 0; i < project.tasks.length; i++) {
+        if (project.tasks[i].completionStatus) {
+            project.tasks.splice(i, 1);
+        }
+    }
+    return project;
 }
